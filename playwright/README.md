26a26a080b9bf0cde7307a6629e4c952602d0b15
jimtausch1 <jimtausch1@gmail.com>
2025-08-20 11:15:19 -0500

    added playwright and login tests

---
+++ playwright/README.md
@@ -0,0 +1,19 @@
+# Playwright setup and commands
+
+-npm init playwright@latest
+-npx playwright test  
+-npx playwright show-report
+-npx playwright test example.spec.ts
+-npx playwright test -g "has title" --project=chromium
+-npx playwright test --project=chromium
+-npx playwright test --project=chromium --headed
+-npx playwright test --project=chromium --trace on
+-npx playwright test --project=chromium --debug
+-npx playwright test --ui
+
+# Playwright more login test commands
+
+-await page.getByRole('textbox', { name: 'email' }).click()
+-await page.getByLabel('Email').fill(testuser);
+-await page.getByLabel('Password').fill(testPassword);
+-await page.locator('#email').click();
---
+++ playwright/tests/login.spec.ts
@@ -0,0 +1,47 @@
+import { expect, test } from '@playwright/test';
+
+const testuser = process.env.TEST_USER as string;
+const testPassword = process.env.TEST_PASSWORD as string;
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('http://localhost:5000/');
+});
+
+test.describe('login page', () => {
+  test('navigate to login page', async ({ page }) => {
+    await page.getByText('Login / Demo User').click();
+
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Jobify/);
+  });
+
+  test('enter email password then login', async ({ page }) => {
+    await page.getByText('Login / Demo User').click();
+    await page.getByLabel('Email').pressSequentially(testuser, { delay: 100 });
+    await page.getByLabel('Password').pressSequentially(testPassword, { delay: 100 });
+    await page.getByText('Submit').click();
+
+    await expect(page.locator('.logout-btn')).toContainText(/Bubbles McLaughster/);
+    await expect(page.locator('.Toastify')).toContainText(/Login successful/);
+
+    const sidebar = page.locator('.sidebar-container.show-sidebar');
+    await expect(sidebar.locator('.nav-link.active')).toContainText(/add job/);
+  });
+});
+
+// test('has title', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
+
+//   // Expect a title "to contain" a substring.
+//   await expect(page).toHaveTitle(/Playwright/);
+// });
+
+// test('get started link', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
+
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click();
+
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+// });
