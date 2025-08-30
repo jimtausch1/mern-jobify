b505c9ffde37c7dfe5d193ac198b0ebb737636ce
jimtausch1 <44066579+jimtausch1@users.noreply.github.com>
2025-08-30 09:04:27 -0500

    updated playwright trace option

--- playwright.config.ts
+++ playwright.config.ts
@@ -32,11 +32,7 @@ export default defineConfig({
     baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on',
-
-    launchOptions: {
-      headless: process.env.CI === 'true' ? true : false,
-    },
+    trace: 'on-first-retry',
   },
 
   /* Configure projects for major browsers */
