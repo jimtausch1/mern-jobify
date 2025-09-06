f7f4cf7055c5f38f51292363b3c30137bb616101
jimtausch1 <44066579+jimtausch1@users.noreply.github.com>
2025-09-06 09:27:00 -0500

    added vite tests for all jobs page

---
+++ client/src/pages/AllJobs.test.tsx
@@ -0,0 +1,66 @@
+import { QueryClientProvider } from '@tanstack/react-query';
+import { render, screen } from '@testing-library/react';
+import { RouterProvider } from 'react-router-dom';
+import { expect, it } from 'vitest';
+import { getMemoryRouter, queryClient } from '../utils/TestHelper';
+
+import { DashboardProvider } from '../context/DashboardProvider';
+import { mockJobsResponse, mockSearchParams } from '../utils/mocks';
+import AllJobs from './AllJobs';
+
+// Mock the 'react-router-dom' module to replace useNavigate
+vi.mock('react-router-dom', async () => {
+  const actual = await vi.importActual('react-router-dom');
+  return {
+    ...actual,
+    useNavigate: () => vi.fn(), // Return our mock function
+    useLoaderData: vi.fn(() => mockSearchParams),
+  };
+});
+
+// Mock useQuery/useSuspenseQuery to return specific data
+vi.mock('@tanstack/react-query', async () => {
+  const actual = await vi.importActual('@tanstack/react-query');
+  return {
+    ...actual,
+    useQuery: vi.fn(() => ({
+      data: mockJobsResponse,
+      isLoading: false,
+      isError: false,
+    })),
+    useSuspenseQuery: vi.fn(() => ({
+      data: {
+        /* your mocked query data */
+      },
+    })),
+  };
+});
+
+describe('All Jobs Page', () => {
+  it('should correctly render', async () => {
+    const router = getMemoryRouter(['/dashboard/all-jobs'], <AllJobs />);
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <DashboardProvider queryClient={queryClient}>
+          <RouterProvider router={router} />
+        </DashboardProvider>
+      </QueryClientProvider>
+    );
+
+    // Log the DOM tree for debugging
+    screen.debug(undefined, Infinity);
+
+    // Find heading by its text content
+    const searchInput = screen.getByLabelText(/search/i);
+    const totalJobsFound = screen.getByText(/15 jobs found/);
+    // const allJobsLink = screen.getAllByText(/all jobs/i);
+    // const statsLink = screen.getAllByText(/stats/i);
+    // const profileLink = screen.getAllByText(/profile/i);
+
+    // Verify heading exists in document
+    expect(searchInput).toBeInTheDocument();
+    expect(totalJobsFound).toBeInTheDocument();
+    // expect(profileLink[0]).toBeInTheDocument();
+  });
+});
--- client/src/utils/handlers.ts
+++ client/src/utils/handlers.ts
@@ -1,5 +1,5 @@
 import { http, HttpResponse } from 'msw';
-import { mockUser } from './mocks';
+import { mockJobsResponse, mockUser } from './mocks';
 
 export const handlers = [
   http.post('/api/v1/auth/login', () => {
@@ -8,6 +8,11 @@ export const handlers = [
   http.get('/api/v1/users/current-user', () => {
     return HttpResponse.json({
       mockUser,
+    });
+  }),
+  http.get('jobs?search=&jobStatus=all&jobType=all&sort=newest', () => {
+    return HttpResponse.json({
+      mockJobsResponse,
     });
   }),
 ];
